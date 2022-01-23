1eb73eda5105ed7f99c203d5564b8abb101dc548
cinofe <jeraru5178@gmail.com>
2022-01-23 18:38:50 +0900

    Sync

--- new_main.py
+++ new_main.py
@@ -1,5 +1,5 @@
 from multipledispatch import dispatch
-import cv2, os, time as t, numpy as np, matplotlib.pyplot as plt
+import cv2, os, numpy as np
 
 class Del_FiveLine:
     '''
@@ -9,28 +9,29 @@ class Del_FiveLine:
     좌,우 측 픽셀값을 검사해가며 시작 위치를 추정
     이미지 좌측에서 가장 위에 있는 검정 픽셀 검출
     '''
-    def __init__(self,img) -> None:
+    def __init__(self,img):
         self.__origin_img = cv2.imread(r'musicnotes/'+img)
         self.__dst = []
         self.__img = []
         self.hist= []
         self.wpos = []
+        self.__values = {}
         self.__GrayScale()
         self.__h, self.__w = self.__dst.shape
         self.__find_hist()
         self.__findFiveLine()
     
-    def __str__(self) -> str:
+    def __str__(self):
         return f'hist : {self.hist} hist length : {len(self.hist)}\nwpos : {self.wpos} wpos length : {len(self.wpos)}'
     
     # 이미지를 컬러 영상에서 -> 흑백 영상으로 변환
-    def __GrayScale(self) -> None:
+    def __GrayScale(self):
         self.__dst = cv2.cvtColor(self.__origin_img,cv2.COLOR_BGR2GRAY)
         self.__img = self.__dst.copy()
     
     #  모든 이미지 보기
     @dispatch()
-    def show(self) -> None:
+    def show(self):
         cv2.imshow('dst',self.__dst)
         cv2.imshow('img',self.__img)
     
@@ -41,22 +42,18 @@ class Del_FiveLine:
 
     # 악보의 수평 히스토그램을 구하고 그 값중 이미지 너비의 70%이상의
     # 값을 오선으로 판단하고 데이터로 포함
-    def __find_hist(self) -> None:
+    def __find_hist(self):
         for i in range(1,self.__h-1):
             value = 0
             for j in range(1,self.__w-1):
-                if self.__dst[i,j] == self.__dst[i,j-1]:
-                    weight = 1.2
-                else:
-                    weight = 1
                 if self.__dst[i,j] < 245:
-                    value += weight
+                    value += 1
             if value >= (self.__w/100)*70 :
                 self.hist.append(i)
-        
+                self.__values[i] = value
     
     # 악보의 수평 히스토그램을 기반으로 오선의 시작 위치(x축) 추정    
-    def __findFiveLine(self) -> None:
+    def __findFiveLine(self):
         s = (self.__w//100)*5
 
         for h in self.hist:
@@ -64,15 +61,15 @@ class Del_FiveLine:
                 p = 0
                 while self.__dst[h,s-p] < 240:
                     p += 1
-                self.wpos.append((s-p)+1)
+                self.wpos.append(s-p)
             else:
                 p = 0
                 while self.__dst[h,(s+p)] >= 240:
                     p += 1
-                self.wpos.append((s+p)-1)
+                self.wpos.append(s+p)
 
     # 검출된 검정 픽셀 선 삭제 
-    def delete_line(self,whpos) -> None:
+    def delete_line(self,whpos):
         for (x,y) in whpos:
             for i in range(x,self.__w):
                 if self.__img[y-1,i] >= 180:
@@ -80,43 +77,61 @@ class Del_FiveLine:
         self.__Morph(whpos)
 
     # 이미지 이진화
-    def binary(self) -> None:
-        # _, self_dst = cv2.threshold(self.__dst,127,255,cv2.THRESH_OTSU)
-        for i in range(self.__img.shape[0]):
-            for j in range(self.__img.shape[1]):
-                if self.__img[i,j] > 130:
-                    self.__img[i,j] = 255
-                else:
-                    self.__img[i,j] = 0
+    def binary(self, img):
+        _, new_img = cv2.threshold(img,0,255,cv2.THRESH_OTSU)
+        return new_img
                      
     # 모폴로지 사용으로 오선 제거중 사라진 부분 복구
-    def __Morph(self, whpos) -> None:
+    def __Morph(self, whpos):
         morph_img = self.__dst.copy()
-        _, morph_img = cv2.threshold(morph_img,127,255,cv2.THRESH_OTSU)
+        morph_img = self.binary(morph_img)
         kernal_v = np.ones((3,3), np.uint8)
         morph_img = cv2.morphologyEx(morph_img,cv2.MORPH_CLOSE, kernal_v)
-        cv2.imshow('test3',morph_img)
 
         for (_,y) in whpos:
             for i in range(self.__w):
                 if self.__img[y,i] > morph_img[y,i]:
                     self.__img[y,i] = morph_img[y,i]
 
+
+    # 제거된 부분 비율 구하는 부분
+    def find_degree(self, whs):
+        o_img = self.__dst.copy()
+        d_img = self.__img.copy()
+        avrs = []
+        avr = 0
+        for (x,y) in whs:
+            value = 0
+            total = self.__values[y]
+            for i in range(x,total):
+                if d_img[y,i] < 127:
+                    value += 1
+            avr = value/total*100
+            avrs.append(avr)
+            print(f'Total Pixel : {total}, Remained Pixel : {value}, Remained Ratio : {avr:.3f}%')
+        print(f'Total Average : {sum(avrs)/len(avrs):.3f}%')
+                
+
+        
+
 def main():
 
     imgs = os.listdir(r'musicnotes')
-    # for img in imgs:
-    #     DFL = Del_FiveLine(img)
-    #     whpos = list(zip(DFL.wpos,DFL.hist))
-    #     DFL.delete_line(whpos)
-    #     DFL.show(img)
+    for img in imgs:
+        DFL = Del_FiveLine(img)
+        whpos = list(zip(DFL.wpos,DFL.hist))
+        DFL.delete_line(whpos)
+        DFL.show(img)
+        DFL.find_degree(whpos)
+        cv2.waitKey()
 
-    DFL = Del_FiveLine(imgs[0])
-    whpos = list(zip(DFL.wpos,DFL.hist))
-    DFL.delete_line(whpos)
-    DFL.show()
+    # DFL = Del_FiveLine(imgs[3])
+    # whpos = list(zip(DFL.wpos,DFL.hist))
+    # DFL.delete_line(whpos)
+    # DFL.show()
+    # DFL.find_degree(whpos)
 
-    cv2.waitKey()
+    # cv2.waitKey()
     
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -1,11 +1,4 @@
-def one_num(n):
-    c = []
-    for i in range(1,n+1):
-        d = set()
-        l = len(s:=str(i))
-        if l > 1:
-            for j in range(l-1):d.add(map(int,s[j:j+1]))   
-            if len(d) == 1:c.append(i)
-        else:c.append(i)
-    print(len(c))
-one_num(int(input()))
+def test(a) -> int:
+    return 'abc'
+
+print(test(2))
