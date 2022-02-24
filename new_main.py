7e9f5a20dcb3c052a225eee3f17d5d2b223f9853
Cinofe <jeraru5178@gmail.com>
2022-02-24 15:55:38 +0900

    Sync

--- new_main.py
+++ new_main.py
@@ -11,13 +11,14 @@ class Del_FiveLine:
     '''
     def __init__(self,img):
         self.__origin_img = cv2.imread(r'SheetMusics/'+img)
+        self.__src = []
         self.__dst = []
         self.__img = []
         self.hist= []
         self.wpos = []
         self.__values = {}
         self.__GrayScale()
-        self.__h, self.__w = self.__dst.shape
+        self.__h, self.__w = self.__src.shape
         self.__find_hist()
         self.__findFiveLine()
     
@@ -29,19 +30,19 @@ class Del_FiveLine:
     
     # 이미지를 컬러 영상에서 -> 흑백 영상으로 변환
     def __GrayScale(self):
-        self.__dst = cv2.cvtColor(self.__origin_img,cv2.COLOR_BGR2GRAY)
-        self.__img = self.__dst.copy()
+        self.__src = cv2.cvtColor(self.__origin_img,cv2.COLOR_BGR2GRAY)
+        self.__dst = self.__src.copy()
     
     #  모든 이미지 보기
     @dispatch()
     def show(self):
-        cv2.imshow('dst',self.__dst)
-        cv2.imshow('img',self.__img)
+        cv2.imshow('dst',self.__src)
+        cv2.imshow('img',self.__dst)
     
     # 결과 이미지 보기
     @dispatch(str)
     def show(self, name):
-        cv2.imshow(name,self.__img)
+        cv2.imshow(name,self.__dst)
 
     # 악보의 수평 히스토그램을 구하고 그 값중 이미지 너비의 70%이상의
     # 값을 오선으로 판단하고 데이터로 포함
@@ -51,7 +52,7 @@ class Del_FiveLine:
         for i in range(1,self.__h-1):
             value = 0
             for j in range(1,self.__w-1):
-                if self.__dst[i,j] <= 250:
+                if self.__src[i,j] <= 250:
                     value += 1
             if value >= (self.__w/100)*70 :
                 self.hist.append(i)
@@ -67,22 +68,23 @@ class Del_FiveLine:
         miny = min(self.hist)
         maxy = max(self.hist)
         avr = (self.__h//100)*5
-        self.__img = self.__img[miny-avr:maxy+avr,0:self.__w].copy()
         self.__dst = self.__dst[miny-avr:maxy+avr,0:self.__w].copy()
+        self.__src = self.__src[miny-avr:maxy+avr,0:self.__w].copy()
+        self.__img = self.__img[miny-avr:maxy+avr,0:self.__w].copy()
 
     # 악보의 수평 히스토그램을 기반으로 오선의 시작 위치(x축) 추정    
     def __findFiveLine(self):
         s = (self.__w//100)*5
 
         for h in self.hist:
-            if self.__dst[h,s] < 240:
+            if self.__src[h,s] < 240:
                 p = 0
-                while self.__dst[h,s-p] < 240:
+                while self.__src[h,s-p] < 240:
                     p += 1
                 self.wpos.append(s-p)
             else:
                 p = 0
-                while self.__dst[h,(s+p)+100] >= 240:
+                while self.__src[h,(s+p)+100] >= 240:
                     p += 1
                 self.wpos.append(s+p)
 
@@ -90,8 +92,8 @@ class Del_FiveLine:
     def delete_line(self,whpos):
         for (x,y) in whpos:
             for i in range(x,self.__w):
-                if self.__img[y-1,i] >= 180:
-                    self.__img[y,i] = 255
+                if self.__dst[y-1,i] >= 180:
+                    self.__dst[y,i] = 255
         self.__Morph(whpos)
         # self.__delete_Name()
 
@@ -102,20 +104,20 @@ class Del_FiveLine:
                      
     # 모폴로지 사용으로 오선 제거중 사라진 부분 복구
     def __Morph(self, whpos):
-        morph_img = self.__dst.copy()
+        morph_img = self.__src.copy()
         morph_img = self.__binary(morph_img)
         kernal_v = np.ones((3,3), np.uint8)
         morph_img = cv2.morphologyEx(morph_img,cv2.MORPH_CLOSE, kernal_v)
 
         for (_,y) in whpos:
             for i in range(self.__w):
-                if self.__img[y,i] > morph_img[y,i]:
-                    self.__img[y,i] = morph_img[y,i]
+                if self.__dst[y,i] > morph_img[y,i]:
+                    self.__dst[y,i] = morph_img[y,i]
 
     # 제거된 부분 비율 구하는 부분
     def find_degree(self, whs):
-        o_img = self.__dst.copy()
-        d_img = self.__binary(self.__img.copy())
+        o_img = self.__src.copy()
+        d_img = self.__binary(self.__dst.copy())
         avrs = []
         avr = 0
         for (x,y) in whs:
@@ -128,32 +130,6 @@ class Del_FiveLine:
             avrs.append(avr)
         print(f'Total Staff-line Pixel : {total}, Minimum Remained Ratio : {min(avrs):.3f}%')
     
-    # 음표 및 여러 객체 외각선 탐지
-    def find_Contours(self):
-        '''
-        1. 음표 인식 방법 모폴로지 침식, 팽창을 이용해서 음표 또는 빔 부분
-        좌표를 기억해 놨다가, 컨투어 추출 할 때, 해당 좌표를 포함하고 있는
-        부분만 사각형으로 표시 <- 문제점 : 2분 음표 추출 불가
-        '''
-
-        src = self.__binary(self.__dst)
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-    
-        src = cv2.dilate(src, kernel, anchor=(-1,-1),iterations=2)
-        # src = cv2.erode(src, kernel, anchor=(-1,-1),iterations=1)
-        
-        # contours, _ = cv2.findContours(src, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-        # i = 1
-        # for contour in contours:
-        #     x,y,w,h = cv2.boundingRect(contour)
-        #     if w < 9 or h < 9:
-        #         continue
-        #     cv2.rectangle(src,(x,y,w,h),(0,0,0),1)
-        #     cv2.putText(src,str(i),(x,y),cv2.FONT_HERSHEY_COMPLEX, 0.5, (0,0,0),1)
-        #     i += 1
-        cv2.imshow('origin',self.__dst)
-        cv2.imshow('find contour',src)
-
     # 오선 사이 사이의 가사 또는 잡음 제거
     '''
     2. 오선이 있는 이미지를 이진화 시킨 후 모폴로지 침식으로 어두운 영역을
@@ -182,23 +158,24 @@ class Del_FiveLine:
         '''
     def delete_noise(self):
         # 기본적인 악절 추적위한 이미지
-        src = self.__binary(self.__dst)
+        dst = self.__binary(self.__src)
         # 악절의 범위 확장을 위한 음표 추적위한 이미지
-        src2 = self.__binary(self.__img)
+        dst2 = self.__binary(self.__dst)
         # 측정된 악절만 이력할 이미지
-        src3 = np.full((self.__h,self.__w),255,np.uint8)
+        dst3 = np.full((self.__h,self.__w),255,np.uint8)
+        dst4 = np.full((self.__h,self.__w),255,np.uint8)
         # 악절의 범위를 명확하게 파악하기 위해 형태학(모폴로지)연산 이용
-        src = cv2.erode(src, np.ones((3,3), np.uint8),anchor=(-1,-1),iterations=1)
-        src = cv2.dilate(src, np.ones((1,5), np.uint8), anchor=(-1,-1),iterations=1)
+        dst = cv2.erode(dst, np.ones((3,3), np.uint8),anchor=(-1,-1),iterations=1)
+        dst = cv2.dilate(dst, np.ones((1,5), np.uint8), anchor=(-1,-1),iterations=1)
         # 명확한 음계를 파악하기 위해 형태학(모폴로지)연산 이용
-        src2 = cv2.erode(src2, np.ones((2,2),np.uint8),anchor=(-1,-1),iterations=1)
+        dst2 = cv2.erode(dst2, np.ones((2,2),np.uint8),anchor=(-1,-1),iterations=1)
         # 악절이 표현된 윤곽 사각형의 좌표가 들어갈 리스트
         FLlocs = []
         # 기호가 표현된 윤곽 사각형의 좌표가 들어갈 리스트
         SymbolLocs = []
         # 영역의 윤곽을 찾는 연산
-        Fcontours, _ = cv2.findContours(src,cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-        Scontours, _ = cv2.findContours(src2, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+        Fcontours, _ = cv2.findContours(dst,cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+        Scontours, _ = cv2.findContours(dst2, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
         
         # 악절 영역 찾기
         for contour in Fcontours:
@@ -217,7 +194,8 @@ class Del_FiveLine:
             if w < 10 or h < 20 or h > 70 or (w > 30 and h < 30):
                 continue
             SymbolLocs.append((x,y,w,h))
-            cv2.rectangle(src2,(x,y),(x+w,y+h),(0,0,0),1)
+            cv2.rectangle(dst2,(x,y),(x+w,y+h),(0,0,0),1)
+        # cv2.imshow('detect symbol',dst2)
 
         # 오선 확장 조건
         # 1. y 조건 - 기호 영역의 y축이 오선의 y축보다 작으면서 기호영역의 y+h가 오선 영역의 y보다 크면 오선 영역의 y를 기호 영역의 y까지 확장.
@@ -238,33 +216,69 @@ class Del_FiveLine:
         # 확장된 악절 영역을 새로운 이미지에 그려주는 작업
         for flloc in FLlocs:
             x,y,w,h = flloc
-            src3[y:y+h,x:x+w] = self.__dst[y:y+h,x:x+w].copy()
-            cv2.rectangle(self.__dst,flloc,(0,0,0),1)
+            dst3[y:y+h,x:x+w] = self.__src[y:y+h,x:x+w].copy()
+            dst4[y:y+h,x:x+w] = self.__dst[y:y+h,x:x+w].copy()
+            cv2.rectangle(self.__src,flloc,(0,0,0),1)
+        
+        self.__img = dst3.copy()
+        self.__dst = dst4.copy()
         """
         제목, 가사들은 제거 완료, 자잘한 노이즈 제거 방법 필요
         노이즈 제거 할때 오선 내부의 음표도 함꼐 지워지는 부분 수정 필요.
         1. 오선 제거된 영상에서 윤관석 검출을 통해 제거해보기
         -> 안됨. 오선과 함께 온음표의 위와 아래가 같이 잘려있어서 노이즈로 처리해버림.
+        
+        일단 세세한 잡음제거는 일시 정지
+         - 세세한 잡음은 무시하고 노래 제목, 작사, 작곡, 가사 등의 큰 잡음만 제거 후
+           기호를 찾고 잘라서 분류하는것 부터 진행.
         """
-        src4 = src3.copy()
-        src4 = self.__binary(src4)
-        # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1,1))
-        src4 = cv2.erode(src4, np.ones((1,1),np.uint8), anchor=(-1,-1),iterations=1)
-        # cv2.imshow('test2',src4)
-        contours, _ = cv2.findContours(src4, cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
+        # src4 = src3.copy()
+        # src4 = self.__binary(src4)
+        # # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1,1))
+        # src4 = cv2.erode(src4, np.ones((1,1),np.uint8), anchor=(-1,-1),iterations=1)
+        # # cv2.imshow('test2',src4)
+        # contours, _ = cv2.findContours(src4, cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
+        # for contour in contours:
+        #     x,y,w,h = cv2.boundingRect(contour)
+        #     if x == 0 and y == 0:
+        #         continue
+        #     if (w < 13 and h < 11) and (w > 5 or h > 5):
+        #         src3[y:y+h, x:x+w] = np.full((h,w),255,np.uint8)
+        #         cv2.rectangle(src4, (x,y,w,h),(0,0,0),1)
+
+        self.__delete_Name()
+
+        # cv2.imshow('detect not noise',self.__src)
+        # cv2.imshow('delete five',self.__dst)
+        # cv2.imshow('delete noise',self.__img)
+
+# 음표 및 여러 객체 외각선 탐지
+    def find_Contours(self):
+        # src = self.__binary(self.__dst)
+        src = cv2.adaptiveThreshold(self.__dst,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,23,2)
+        src2 = self.__img.copy()
+
+        src = cv2.erode(src,np.ones((1,2),np.uint8),anchor=(-1,-1),iterations=1)
+
+        cv2.imshow('adaptive threshold',src)
+        contours, _ = cv2.findContours(src,cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
+        """
+        윤곽선만 잡아서 이미지를 잘라내려고 했는데 언떤결에 세세한 잡음을 어느정도 제거하는데 성공함
+        -> 다른 이미지에서도 되는지 확인 필요 ->
+        """
         for contour in contours:
             x,y,w,h = cv2.boundingRect(contour)
-            if x == 0 and y == 0:
+            if x == 0 or y == 0:
                 continue
-            if (w < 13 and h < 11) and (w > 5 or h > 5):
-                src3[y:y+h, x:x+w] = np.full((h,w),255,np.uint8)
-                cv2.rectangle(src4, (x,y,w,h),(0,0,0),1)
+            if w > 7 and w < 50 and h < 10:
+                src[y:y+h,x:x+w] = np.full((h,w),255,np.uint8).copy()
+            elif w < 20 and h < 5:
+                src[y:y+h,x:x+w] = np.full((h,w),255,np.uint8).copy()
+            # else : 
+            #     cv2.rectangle(src,(x,y,w,h),(0,0,0),1)
+
+        cv2.imshow('find contour',src)
         
-        cv2.rectangle(src4,(0,0,13,11),(0,0,0),1)
-        cv2.rectangle(src4,(0,12,5,5),(0,0,0),1)
-        cv2.imshow('delete noise',self.__dst)
-        cv2.imshow('delete five',self.__img)
-        cv2.imshow('src',src3)
 
 def allimg():
     imgs = os.listdir(r'SheetMusics')
@@ -278,24 +292,25 @@ def allimg():
         DFL.show(img)
         # DFL.find_degree(whpos)
         DFL.delete_noise()
+        DFL.find_Contours()
         cv2.waitKey()
     
 def oneimg():
     imgs = os.listdir(r'SheetMusics')
-    DFL = Del_FiveLine(imgs[5])
+    DFL = Del_FiveLine(imgs[0])
     whpos = list(zip(DFL.wpos,DFL.hist))
     DFL.delete_line(whpos)
     # DFL.show()
     # DFL.find_degree(whpos)
-    # DFL.find_Contours()
     DFL.delete_noise()
+    DFL.find_Contours()
 
     cv2.waitKey()
 
 
 def main():
-    oneimg()
-    # allimg()
+    # oneimg()
+    allimg()
     
 if __name__ == '__main__':
     main()
